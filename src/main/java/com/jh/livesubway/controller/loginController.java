360105c7effec20434b2b29e9881a7283b201129
Jeonghun HAN <101242920+Hisabori@users.noreply.github.com>
2023-02-26 16:49:09 +0900

    KauthService 구현중 코드 오류 정리중
    주말 코드 수정중

--- src/main/java/com/jh/livesubway/controller/loginController.java
+++ src/main/java/com/jh/livesubway/controller/loginController.java
@@ -10,9 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Serial;
 
 @WebServlet(name = "LoginController", value = "/LoginController")
 public class loginController extends HttpServlet {
+
 
 
 
